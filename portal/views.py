4748ed6d2524707bdcc87ea6ad1e13ae1f05fc2b
sharat910 <sharat910@gmail.com>
2016-07-07 13:49:00 +0530

    Added Algo support

--- portal/views.py
+++ portal/views.py
@@ -2,27 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response,render
 from django.http import HttpResponseRedirect,Http404,HttpResponse
 import json,random
-import matplotlib.pyplot as plt
-import datacube
 from .forms import *
 from django.template import RequestContext
 
+#Datacube Imports
+import matplotlib.pyplot as plt
+import datacube
+
+#Imports for RVI algo
+import numpy
+from datetime import datetime
+from datacube.analytics.analytics_engine import AnalyticsEngine
+from datacube.execution.execution_engine import ExecutionEngine
+from datacube.analytics.utils.analytics_utils import plot
+
+#Imports for NDVI algo
+import xarray as xr
+from datacube.api import API
+from datacube.ndexpr import NDexpr
+
+
 # Create your views here.
 def Home(request):
-	form = dataForm(request.POST or None)		
-	if form.is_valid():
-		year = request.POST['year']
-		layer = request.POST['layer']
-		x1 = float(request.POST['x1'])
-		x2 = float(request.POST['x2'])
-		y1 = float(request.POST['y1'])
-		y2 = float(request.POST['y2'])
-		imsource = plot(year,layer,x1,x2,y1,y2)
-	else:
-		imsource = ""
+	form = dataForm(request.POST or None)
+	imsource = ''
 	context = {'imsource':imsource,
-					'form':form}
-	print context
+				'form':form}
 	return render_to_response("four.html",context,context_instance = RequestContext(request))
 
 def AJAXHandle(request):
@@ -31,98 +36,166 @@ def AJAXHandle(request):
 	if request.is_ajax():
 		print request.POST
 		print "\n\n\n","HEERRRERRER","\n\n\n"	
-		year = request.POST['year']
+		year = int(request.POST['year'])
 		layer = request.POST['layer']
 		epsg = request.POST['epsg']
-		algo = request.POST['prod']
+		prod = request.POST['prod']
+		algo = request.POST['algo']
 		print "before truncating"
 		x1 = float(request.POST['x1'])
 		x2 = float(request.POST['x2'])
 		y1 = float(request.POST['y1'])
 		y2 = float(request.POST['y2'])
 		print "before call"
-		imsource = plot(year,layer,x1,x2,y1,y2,epsg,algo)
+		imsource = plot(year,layer,x1,x2,y1,y2,epsg,prod,algo)
 		response = {'status': 1, 'message': imsource + "?t=" + str(random.randint(1,1000))} 
 	else:
 		response = {'status': 0, 'message': "error"} 
 	return HttpResponse(json.dumps(response), content_type='application/json')
 
-def plot(year,layer,x1,x2,y1,y2,epsg,algo):
+def plot(year,layer,x1,x2,y1,y2,epsg,prod,algo):
 	print "In Plot"
 	print x1,x2,y1,y2
 	plt.hold(False)
 	##
 	dc = datacube.Datacube(config="/home/sharat910/.datacube.conf")
 	##
-	if algo == "Reflectance":	
-		la = dc.load(product='ls5_ledaps_albers', x=(x1, x2), y=(y1,y2))
-		print la
-		if layer == "1":
-			a = la.blue
-			a = a.where(a != a.attrs['nodata'])
-		elif layer == "2":
-			a = la.green
-			a = a.where(a != a.attrs['nodata'])		
-		elif layer == "3":
-			a = la.red
-			a = a.where(a != a.attrs['nodata'])
-		elif layer == "4":
-			a = la.nir
-			a = a.where(a != a.attrs['nodata'])
-		print str(year)
-		a = a.loc[str(year)]
-		a.plot()
-		path = 'media/images/' +  str(year) + '.png'
-		plt.savefig(path)
-		plt.clf()
-		return path
+	if prod == "Reflectance":		
+		if algo == "None":	
+			return Reflectance(x1,x2,y1,y2,year,layer)
+		elif algo == "NDVI":			
+			return NDVI(x1,x2,y1,y2,year)
+		elif algo == 'RVI' or algo == 'TVI':
+			return RVIorTVI(x1,x2,y1,y2,year,algo)
+		else:
+			return ''
 
-	elif algo == "NDVI":
-		import xarray as xr
-		from datetime import datetime
-		from datacube.api import API
-		from datacube.ndexpr import NDexpr
-		year = int(year)
-		g = API()
-		nd = NDexpr()
+def Reflectance(x1, x2, y1, y2, year,layer):
+	dc = datacube.Datacube(config="/home/sharat910/.datacube.conf")
+	la = dc.load(product=get_product(year), x=(x1, x2), y=(y1,y2))
+	print la
+	if layer == "1":
+		a = la.blue
+		a = a.where(a != a.attrs['nodata'])
+	elif layer == "2":
+		a = la.green
+		a = a.where(a != a.attrs['nodata'])		
+	elif layer == "3":
+		a = la.red
+		a = a.where(a != a.attrs['nodata'])
+	elif layer == "4":
+		a = la.nir
+		a = a.where(a != a.attrs['nodata'])
+	print str(year)
+	a = a.loc[str(year)]
+	a.plot()
+	path = 'media/images/' +  str(year) + '.png'
+	plt.savefig(path)
+	plt.clf()
+	return path
 
-		satellite = get_sat(year)
+def NDVI(x1, x2, y1, y2, year):	
+	g = API()
+	nd = NDexpr()
 
-		data_request_descriptor = {
-    		'platform': satellite,
-    		'product': 'ledaps',
-    		'variables': ('red', 'nir'),
-    		'dimensions': {
-        		'x': {
-            		'range': (x1,x2)
-       			},
-        		'y': {
-        	    'range': (y1,y2)
-       			},
-        		'time': {
-            		'range': (datetime(year, 1, 1), datetime(year, 12, 31))
-        		}
+	satellite = get_sat(year)
+
+	data_request_descriptor = {
+		'platform': satellite,
+		'product': 'ledaps',
+		'variables': ('red', 'nir'),
+		'dimensions': {
+    		'x': {
+        		'range': (x1,x2)
+   			},
+    		'y': {
+    	    'range': (y1,y2)
+   			},
+    		'time': {
+        		'range': (datetime(year, 1, 1), datetime(year, 12, 31))
     		}
 		}
+	}
 
-		# Retrieving data from API
-		d1 = g.get_data(data_request_descriptor)
+	# Retrieving data from API
+	d1 = g.get_data(data_request_descriptor)
 
-		b30 = d1['arrays']['red']
-		b40 = d1['arrays']['nir']
+	b30 = d1['arrays']['red']
+	b40 = d1['arrays']['nir']
 
-		ndvi = nd.evaluate('((b40 - b30) / (b40 + b30))')
+	ndvi = nd.evaluate('((b40 - b30) / (b40 + b30))')
 
-		ndvi.plot()
+	ndvi.plot()
 
-		path = 'media/images/' +  'ndvi-' + str(year) + '.png'
-		plt.savefig(path)
-		plt.clf()
-		return path
+	path = 'media/images/' +  'ndvi-' + str(year) + '.png'
+	plt.savefig(path)
+	plt.clf()
+	return path
+
+def RVIorTVI (x1, x2, y1, y2, year,algo):
+	dc = datacube.Datacube(config="/home/sharat910/.datacube.conf")	
+	
+	a = AnalyticsEngine()	
+	e = ExecutionEngine()
+
+	product = get_product(year)
+	#assert product
+	#dc.list_products() is pandas dataframe, .loc[:]['name'] selects
+	#   the products is a pandas series. values is array
+	assert (product in dc.list_products().loc[:]['name'].values), "Product not in database"
+
+	# 2 index for platform, 3 for product type
+	for prod  in dc.list_products().loc[:].values:
+	    if(product == prod[0]):
+	        platform = prod[2]
+	        product_type = prod[3]
+
+	
+	# assert time
+	la = dc.load(product=product, x = (x1, x2),y = (y1, y2))
+	# this is date of product (la.items()[0])[1].values[0]
+	date_of_prod = (la.items()[0])[1].values[0]
+	#this is numpy.datetime64
+
+	# TO DO COMPARE numpy.datetime64 with datetime.datetime    
+	#assert (date_of_prod >= time1 and date_of_prod <= time2), "Product not in the provided time"
+
+	time1 = datetime(year, 1, 1)
+	time2 = datetime(year, 12, 31)
+	# calculate output
+	dimensions = {'x':    {'range': (x1, x2)},
+	              'y':    {'range': (y1, y2)},
+	              'time': {'range': (time1, time2)}}
+
+	# create arrays
+	b40 = a.create_array((platform, product_type), ['nir'], dimensions, 'b40')
+	b30 = a.create_array((platform, product_type), ['red'], dimensions, 'b30')
+
+	if algo == 'RVI':
+		#ratio vegetation index
+		rvi = a.apply_expression([b40, b30], '(array1 / array2)', 'rvi')
+
+		e.execute_plan(a.plan)
+
+		#result x array
+		res = e.cache['rvi']['array_result']['rvi']
+		res.plot()
+		path = 'media/images/' +  'rvi-' + str(year) + '.png'
+	elif algo == 'TVI':
+		#Transformed Vegetation Index
+		tvi = a.apply_expression([b40, b30], '(sqrt(((array1 - array2) / (array1 + array2)) + 0.5) * 100)', 'tvi')
+
+		e.execute_plan(a.plan)
+
+		#result x array
+		res = e.cache['tvi']['array_result']['tvi']
+		res.plot()
+		path = 'media/images/' +  'tvi-' + str(year) + '.png'
 	else:
-		return ''
-
-
+		pass 	
+	plt.savefig(path)
+	plt.clf()
+	return path
 
 def reductionTime(start_year,end_year,operation):
 	from datetime import datetime
@@ -141,6 +214,14 @@ def reductionTime(start_year,end_year,operation):
 
 	median = a.apply_expression(arrays, operation +'(array1, 0)', 'medianT')
 
+def get_product(year):	
+	if year<=1995 or year == 2010:
+		product = 'ls5_ledaps_albers'
+	elif year<=2003:
+		product = 'ls7_ledaps_albers'
+	elif year == 2015:
+		product = 'ls8_ledaps_albers'
+	return product
 
 def get_sat(year):
 	if year<=1995 or year == 2010:
